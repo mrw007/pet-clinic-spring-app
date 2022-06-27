1ad4fbd663377c6458a7049a1f170097c656b9ea
Wahib Kerkeni <mr.wahib@gmail.com>
2022-06-27 12:10:56 +0100

    Adding POJOs. Closes #2

---
+++ src/main/java/mrw007/springframework/petclinicspringapp/model/Owner.java
@@ -0,0 +1,4 @@
+package mrw007.springframework.petclinicspringapp.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/mrw007/springframework/petclinicspringapp/model/Vet.java
@@ -0,0 +1,4 @@
+package mrw007.springframework.petclinicspringapp.model;
+
+public class Vet extends Person{
+}
