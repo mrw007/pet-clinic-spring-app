03fdb3244705e49bc50bc6112ad268e0f2db54d4
Wahib Kerkeni <mr.wahib@gmail.com>
2022-06-27 15:19:27 +0100

    Adding Base Entity. Closes #10

--- pet-clinic-data/src/main/java/mrw007/springframework/petclinicspringapp/model/Person.java
+++ pet-clinic-data/src/main/java/mrw007/springframework/petclinicspringapp/model/Person.java
@@ -1,6 +1,6 @@
 package mrw007.springframework.petclinicspringapp.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/mrw007/springframework/petclinicspringapp/model/PetType.java
+++ pet-clinic-data/src/main/java/mrw007/springframework/petclinicspringapp/model/PetType.java
@@ -1,6 +1,6 @@
 package mrw007.springframework.petclinicspringapp.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
