79da8714d8b2df51ead3321b20fe8a360a2234c5
Wahib Kerkeni <mr.wahib@gmail.com>
2022-06-28 14:15:57 +0100

    Implemented Map Based Services. Closes #5

---
+++ pet-clinic-data/src/main/java/mrw007/springframework/petclinicspringapp/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package mrw007.springframework.petclinicspringapp.services.map;
+
+import mrw007.springframework.petclinicspringapp.model.Pet;
+import mrw007.springframework.petclinicspringapp.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
