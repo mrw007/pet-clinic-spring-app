0b28a5b187fc06e7925bf4057eb1d6126f230fb2
Wahib Kerkeni <mr.wahib@gmail.com>
2022-07-12 13:42:28 +0100

    Added PetTypeService. Closes #30

---
+++ pet-clinic-data/src/main/java/mrw007/springframework/petclinicspringapp/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package mrw007.springframework.petclinicspringapp.services;
+
+import mrw007.springframework.petclinicspringapp.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
