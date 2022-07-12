fdcf773c62ccdaf78c665122528634462e9bb690
Wahib Kerkeni <mr.wahib@gmail.com>
2022-07-12 14:25:42 +0100

    Added Specialties Map Impl. Closes #32

---
+++ pet-clinic-data/src/main/java/mrw007/springframework/petclinicspringapp/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package mrw007.springframework.petclinicspringapp.services;
+
+import mrw007.springframework.petclinicspringapp.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long>{
+}
