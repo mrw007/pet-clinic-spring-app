95b048ecdba25ce3a5aa323e12220b77e4785fcd
Wahib Kerkeni <mr.wahib@gmail.com>
2022-07-16 17:22:39 +0100

    Added Map impl for Visit Service. Closes #47

---
+++ pet-clinic-data/src/main/java/mrw007/springframework/petclinicspringapp/services/VisitService.java
@@ -0,0 +1,6 @@
+package mrw007.springframework.petclinicspringapp.services;
+
+import mrw007.springframework.petclinicspringapp.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
