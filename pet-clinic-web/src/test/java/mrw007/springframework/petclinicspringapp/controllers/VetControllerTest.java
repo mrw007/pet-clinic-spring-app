b434fdd57c9fe14b13548bd596f5e57a832a2791
Wahib Kerkeni <mr.wahib@gmail.com>
2022-07-20 14:50:50 +0100

    Added Spring MVC Tests for Vet Controller. Closes #74

---
+++ pet-clinic-web/src/test/java/mrw007/springframework/petclinicspringapp/controllers/VetControllerTest.java
@@ -0,0 +1,54 @@
+package mrw007.springframework.petclinicspringapp.controllers;
+
+import mrw007.springframework.petclinicspringapp.model.Vet;
+import mrw007.springframework.petclinicspringapp.services.VetService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class VetControllerTest {
+
+    @Mock
+    VetService vetService;
+
+    @InjectMocks
+    VetController controller;
+
+    MockMvc mockMvc;
+
+    Set<Vet> vets;
+
+    @BeforeEach
+    void setUp() {
+        vets = new HashSet<>();
+        vets.add(Vet.builder().id(1L).build());
+        vets.add(Vet.builder().id(2L).build());
+
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void listVets() throws Exception {
+        when(vetService.findAll()).thenReturn(vets);
+        mockMvc.perform(get("/vets"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("vets/index"))
+                .andExpect(model().attribute("vets", hasSize(2)));
+
+    }
+}
